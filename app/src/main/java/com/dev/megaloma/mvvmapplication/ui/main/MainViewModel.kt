e2d3f46dedebe4a48a71495f2e5df51d6869c706
tomoki10 <tomoki.yakinikuteisyoku@gmail.com>
2019-04-23 21:53:42 +0900

    ObservableFieldからLiveDataに実装を変更

--- app/src/main/java/com/dev/megaloma/mvvmapplication/ui/main/MainViewModel.kt
+++ app/src/main/java/com/dev/megaloma/mvvmapplication/ui/main/MainViewModel.kt
@@ -1,34 +1,27 @@
 package com.dev.megaloma.mvvmapplication.ui.main
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.databinding.BindingAdapter
-import android.databinding.ObservableField
 import android.widget.ImageView
 
 class MainViewModel : ViewModel() {
-    var name: ObservableField<String> = ObservableField()
+
+    private var mName: MutableLiveData<String> = MutableLiveData()
+    var name: LiveData<String> = mName
     fun setName(name :String){
-        this.name.set(name)
+        mName.postValue(name)
     }
-    var cityName: ObservableField<String> = ObservableField()
+
+    private var mCityName: MutableLiveData<String> = MutableLiveData()
+    var cityName: LiveData<String> = mCityName
     fun setCityName(cityName :String){
-        this.cityName.set(cityName)
+        mCityName.postValue(cityName)
     }
 
     @BindingAdapter("android:src")
     fun setImageViewResource(imageView: ImageView, res: Int){
         imageView.setImageResource(res)
     }
-
-
-//    private var _cityName: MutableLiveData<String> = MutableLiveData()
-//    val cityName: LiveData<String>
-//        get() = _cityName
-//    fun setCityName(cityName: String){
-//        val update = cityName
-//        if(_cityName.value == (update)){
-//            return
-//        }
-//        _cityName.value = update
-//    }
 }
