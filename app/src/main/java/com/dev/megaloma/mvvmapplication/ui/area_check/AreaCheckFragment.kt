41eff05421dd2ed5942aa0eabce30a6c45e30408
tomoki10 <tomoki.yakinikuteisyoku@gmail.com>
2019-04-30 22:02:45 +0900

    AreaCheckをViewModel化

--- app/src/main/java/com/dev/megaloma/mvvmapplication/ui/area_check/AreaCheckFragment.kt
+++ app/src/main/java/com/dev/megaloma/mvvmapplication/ui/area_check/AreaCheckFragment.kt
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.dev.megaloma.mvvmapplication.R
 import com.dev.megaloma.mvvmapplication.databinding.AreaCheckFragmentBinding
 import java.util.*
 
@@ -27,9 +28,13 @@ class AreaCheckFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        mView = inflater.inflate(com.dev.megaloma.mvvmapplication.R.layout.area_check_fragment, container, false)
+        mView = inflater.inflate(R.layout.area_check_fragment, container, false)
 
         val binding: AreaCheckFragmentBinding = AreaCheckFragmentBinding.bind(mView)
+        viewModel = ViewModelProviders.of(this).get(AreaCheckViewModel::class.java)
+        binding.setLifecycleOwner(this)
+        binding.viewmodel = viewModel
+
         // レイアウトマネージャを設定(ここで縦方向の標準リストであることを指定)
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
         // Adapterの設定
@@ -38,22 +43,9 @@ class AreaCheckFragment : Fragment() {
         //境界線の描画
         val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
         binding.recyclerView.addItemDecoration(dividerItemDecoration)
-        
-        // RecyclerViewの参照を取得
-        //mRecyclerView = mView!!.findViewById(com.dev.megaloma.mvvmapplication.R.id.recycler_view)
-        // レイアウトマネージャを設定(ここで縦方向の標準リストであることを指定)
-        //mRecyclerView!!.layoutManager = LinearLayoutManager(context)
-        //境界線の描画
-//        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
-//        mRecyclerView!!.addItemDecoration(dividerItemDecoration)
-
-        // Adapterの設定
-//        val recyclerViewAdapter =  RecyclerViewAdapter(convertArrayToList(com.dev.megaloma.mvvmapplication.R.array.prefecture_names))
-//        mRecyclerView!!.adapter = recyclerViewAdapter
 
         recyclerViewAdapter.setOnItemClickListener(object : RecyclerViewAdapter.onItemClickListener {
             override fun onClick(view: View, name: String) {
-                Log.d("Test","$name clicked")
                 // クリックしたアイテムの位置を取得
                 val itemId = resources.getIdentifier(name,"string",context!!.packageName)
 
@@ -77,21 +69,21 @@ class AreaCheckFragment : Fragment() {
                             val cityCode=
                             TransrateCityNameToCityCode().convert(
                                     items.get(checkedItem)
-                                    ,resources.getStringArray(com.dev.megaloma.mvvmapplication.R.array.city_names)
-                                    ,resources.getIntArray(com.dev.megaloma.mvvmapplication.R.array.city_code).toTypedArray()
-                            ) ?: resources.getIntArray(com.dev.megaloma.mvvmapplication.R.array.city_code)[0]
+                                    ,resources.getStringArray(R.array.city_names)
+                                    ,resources.getIntArray(R.array.city_code).toTypedArray()
+                            ) ?: resources.getIntArray(R.array.city_code)[0]
 
                             val data = context!!.getSharedPreferences("DataSave", Context.MODE_PRIVATE)
                             val editor = data.edit()
                             editor.putInt("city_code", cityCode)
                             editor.apply()
-                            Log.d("res",resources.getIntArray(com.dev.megaloma.mvvmapplication.R.array.city_code)[1].toString())
+                            Log.d("res",resources.getIntArray(R.array.city_code)[0].toString())
                             Log.d("city_code put",cityCode.toString())
 
                             Optional.ofNullable(activity)
                                     .filter { activity -> activity is OnFragmentListener }
                                     .map { activity -> activity as OnFragmentListener }
-                                    .orElseThrow { IllegalStateException("ActivityにOnFragmentListenerを実装してください") }
+                                    .orElseThrow { IllegalStateException("要：ActivityにOnFragmentListenerを実装") }
                                     .onFragmentFinish()
                         }
                         .setNegativeButton("Cancel", null)
--- app/src/main/java/com/dev/megaloma/mvvmapplication/ui/area_check/AreaCheckViewModel.kt
+++ app/src/main/java/com/dev/megaloma/mvvmapplication/ui/area_check/AreaCheckViewModel.kt
@@ -1,7 +1,13 @@
 package com.dev.megaloma.mvvmapplication.ui.area_check
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.support.v7.widget.RecyclerView
 
 class AreaCheckViewModel : ViewModel() {
-
+    private var mRecyclerView: MutableLiveData<RecyclerView> = MutableLiveData()
+    var recyclerView: MutableLiveData<RecyclerView> = mRecyclerView
+    fun setRecyclerView(recyclerView :RecyclerView){
+        mRecyclerView.postValue(recyclerView)
+    }
 }
